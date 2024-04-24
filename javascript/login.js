f4bfa30b425395422589368443737c55a70e55bc
luansramos <luanramosgarciadesouza014@gmail.com>
2024-04-24 10:24:21 -0300

    problema de redirecionamento do login resolvido

--- javascript/login.js
+++ javascript/login.js
@@ -3,10 +3,11 @@ function logar() {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if(login == "1234" && senha == "1234"){
-        
+    if(login === "1234" && senha === "1234"){
+        window.location.assign("home.html")
         alert('sucesso');
-        window.location.href = "catalogo.html";
+
+
     }
     else{
         alert('invalido')
